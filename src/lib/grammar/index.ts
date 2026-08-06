0813831b2d83f1d77eb61f3a1512a262416a2955
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-08-06 16:59:50 -0300

    fix: code editor folding

--- src/lib/grammar/index.ts
+++ src/lib/grammar/index.ts
@@ -1,4 +1,4 @@
-import { continuedIndent, foldInside, foldNodeProp, indentNodeProp } from '@codemirror/language';
+import { continuedIndent, foldNodeProp, indentNodeProp } from '@codemirror/language';
 import { parser } from './vinum.grammar';
 
 //TODO: consertar indentação das brackets no final
@@ -13,7 +13,44 @@ const configuredParser = parser.configure({
 			})
 		}),
 		foldNodeProp.add({
-			Body: foldInside
+			DryDefinition(node) {
+				const marker = node.getChild('DeclMarker');
+				const close = node.getChild(']');
+				if (!marker || !close) return null;
+
+				return { from: marker.to, to: close.from };
+			},
+			DryGlobalDefinition(node) {
+				const marker = node.getChild('GlobalDeclMarker');
+				const close = node.getChild(']');
+				if (!marker || !close) return null;
+
+				return { from: marker.to, to: close.from };
+			},
+			DryCall(node) {
+				const identifier = node.getChild('Identifier');
+				const close = node.getChild(']');
+				if (!identifier || !close) return null;
+
+				return { from: identifier.to, to: close.from };
+			},
+			DryComment(node) {
+				const commentTag = node.getChild('#');
+				const close = node.getChild(']');
+				if (!commentTag || !close) return null;
+
+				return { from: commentTag.to, to: close.from };
+			},
+			GroupSweetCall(node) {
+				const open = node.getChild('(');
+				const close = node.getChild(').');
+				if (!open || !close) return null;
+
+				return { from: open.to, to: close.from };
+			},
+			String(node) {
+				return { from: node.from + 2, to: node.to - 2 };
+			}
 		})
 	]
 });
