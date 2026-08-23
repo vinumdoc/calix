c227abf945da23256913a22d58a5b15881396002
Arthur Grillo <arthurgrillo@riseup.net>
2026-08-23 16:04:27 -0300

    routes: Kill legacy document route
    The route is not used anymore, so delete it to avoid confusion

--- src/routes/document/[id]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ params }) => {
-	return {
-		props: {
-			id: params.id
-		}
-	};
-};
--- src/routes/document/[id]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	let { data }: { data: PageData } = $props();
-</script>
-
-<div>este vai ser o doc {JSON.stringify(data)}</div>
